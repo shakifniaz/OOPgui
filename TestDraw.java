3dd1fd155d932d0971ea76cec6ae9c846dbb1f7d
shakifniaz <shakifniaz@gmail.com>
2024-09-22 20:03:29 +0600

    Add files via upload

---
+++ TestDraw.java
@@ -0,0 +1,14 @@
+//8.19
+import javax.swing.JFrame;
+
+public class TestDraw {
+    public static void main(String[] args){
+        DrawPanel1 panel = new DrawPanel1();
+        JFrame app = new JFrame();
+
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.add(panel);
+        app.setSize(300, 300);
+        app.setVisible(true);
+    }
+}
